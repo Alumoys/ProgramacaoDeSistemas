9ab7c01688cf34866ecc7e89e5adceccc94d8f67
Alumoys <128159774+Alumoys@users.noreply.github.com>
2024-04-03 12:06:05 -0300

    Add files via upload

---
+++ Módulo-01/comandoIniciais.py
@@ -0,0 +1,24 @@
+print("Olá, Mundo!")
+###################################
+print("Brasil", "ganhou", 5, "Copas Mundiais!", sep="-")
+###################################
+pais = "Brasil"
+quantidade = 5
+
+print(pais, "ganhou", quantidade, "Títulos-Mundiais!")
+###################################
+type(pais)
+type(quantidade)
+#resultado
+#<class 'str'>
+#<class 'int'>
+######teste-excercícios#######
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
+
+dia = 15
+mes = 10
+ano = 2015
+print(dia, mes, ano, sep="/")
