59f79590546e107a76a8b4c8859c8a08d93f9ea8
Alumoys <128159774+Alumoys@users.noreply.github.com>
2024-04-03 12:17:51 -0300

    Add files via upload

---
+++ Módulo-01/TipagemdoPython.py
@@ -0,0 +1,23 @@
+pais = "Brasil"
+type(pais)
+############################]
+#str pais = "Brasil"
+pais = "Brasil"
+type(pais)
+pais = 644
+type(pais)
+###########################
+preco = 49.99
+type(preco)
+#<class 'float'>
+############
+idade = 12
+###########################
+idade_esposa = 20
+perfil_vip = 'Flávio Almeida'
+recibos_em_atraso = 30
+
+
+idadeEsposa = 20
+perfilVip = 'Flávio Almeida'
+recibosEmAtraso = 30
